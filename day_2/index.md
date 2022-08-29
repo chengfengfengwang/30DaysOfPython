bcb79ad1fdf64c3f2361a3c8da690bced9bf5f9d
wangchengfeng <wangchengfeng@xiaoyezi.com>
2022-08-29 19:04:41 +0800

    day_2

---
+++ day_2/index.md
@@ -0,0 +1,10 @@
+1. python developers use snake case(snake_case) variable naming convention
+2. declare multiple variable in a line
+```
+a, b = 'aa', True
+print(a, b)
+```
+3. built-in functions can use without import and configure like print() len() type()
+4. type casting
+float() int() str()
+list(str) # str to list
---
+++ day_2/variables.py
@@ -0,0 +1,16 @@
+# Day 2:30Days of python programming
+first_name = 'jielun'
+last_name = 'zhou'
+full_name = 'zhoujielun'
+age = 12
+is_married = False
+a, b = 'aa', 'bb'
+num_one, num_two = 5, 4
+
+floor_division = num_one // num_two
+
+country = input('请输入国家:')
+city = input('请输入城市:')
+
+print(country, city, floor_division)
+print(len(first_name))
