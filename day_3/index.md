333f357801178e6ffbb02fa52e77e5eb2ac92c77
wangchengfeng <wangchengfeng@xiaoyezi.com>
2022-09-14 18:28:22 +0800

    day_3

---
+++ day_3/index.md
@@ -0,0 +1,13 @@
+1. operator in: 
+```
+arr = [2, 3, 4]
+str = 'abcdefg'
+print(3 in arr)  
+print('c' in str)
+```
+2. and or not
+```
+a = 3
+print(a > 2 and a < 5)
+print(a > 10 or a < 2)
+```
---
+++ day_3/operators.py
@@ -0,0 +1,44 @@
+# age = 12
+# height=100.72
+# c=4j
+
+# 4
+'''
+base=input("Enter base: ")
+height=input("Enter height: ")
+area=int(base)*int(height)
+print("The area of the triangle is ",area)
+'''
+
+# 5
+# a=input("Enter side a: ")
+# b=input("Enter side b: ")
+# c=input("Enter side c: ")
+# perimeter=int(a)+int(b)+int(c)
+# print("The perimeter of the triangle is ",perimeter)
+
+
+# a=1
+# print(a,a**0,a**1,a**2,a**3)
+# a=2
+# print(a,a**0,a**1,a**2,a**3)
+# a=3
+# print(a,a**0,a**1,a**2,a**3)
+# a=4
+# print(a,a**0,a**1,a**2,a**3)
+# a=5
+# print(a,a**0,a**1,a**2,a**3)
+
+# arr = [2, 3, 4]
+# str = 'abcdefg'
+# print(3 in arr)  
+# print('c' in str)
+
+a = 3
+print(a > 2 and a < 5)
+print(a > 10 or a < 2)
+print(not a > 2)
+b = 1.2
+print(type(b))
+p = 'python'
+print(len(p))
